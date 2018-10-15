9ef7859ba7b610adefe22b0a5a7a675bc0b5b6a9
WojTechM <wojciech.m1997@gmail.com>
2018-10-15 14:00:38 +0200

    Create GoogleSheetLoader class

---
+++ src/main/java/com/codecool/service/fileManagement/GoogleSheetLoader.java
@@ -0,0 +1,11 @@
+package com.codecool.service.fileManagement;
+
+import com.codecool.exception.MalformedQueryException;
+import com.codecool.model.Table;
+
+public class GoogleSheetLoader implements ITableLoader {
+    @Override
+    public Table getTable(String tableName) throws MalformedQueryException {
+        return null;
+    }
+}
