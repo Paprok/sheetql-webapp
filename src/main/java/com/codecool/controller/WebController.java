4b89998c33caf08b3dcd72c485435874f7a0ff6e
saturator22 <f.szczeski@gmail.com>
2018-10-16 12:41:18 +0200

    Remove unused imports in WebController, add maven dependency for executable jar

--- src/main/java/com/codecool/controller/WebController.java
+++ src/main/java/com/codecool/controller/WebController.java
@@ -1,11 +1,7 @@
 package com.codecool.controller;
 
-import com.codecool.model.Entry;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class WebController {
