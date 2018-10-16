30ee7e7d3eb0e2c6684629706ec2cb78d84c4404
Bart <bartpetka@gmail.com>
2018-10-16 11:08:00 +0200

    Implement serveGetRequest method

--- src/main/java/com/codecool/controller/WebController.java
+++ src/main/java/com/codecool/controller/WebController.java
@@ -1,16 +1,36 @@
 package com.codecool.controller;
 
+import com.codecool.exception.MalformedQueryException;
 import com.codecool.model.Entry;
+import com.codecool.model.UserQuery;
+import com.codecool.service.dataManipulation.SelectData;
+import com.codecool.service.queryValidation.QueryValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @Controller
 public class WebController {
+    private SelectData selectData;
+    private QueryValidator queryValidator;
+
+    @Autowired
+    public WebController(SelectData selectData, QueryValidator queryValidator) {
+        this.selectData = selectData;
+        this.queryValidator = queryValidator;
+    }
+
     @GetMapping("/shtql")
-    public String display() {
+    public String serveGetRequest(Model model) {
+        model.addAttribute("userQuery", new UserQuery());
         return "shtql";
     }
+
+
 }
