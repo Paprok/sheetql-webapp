0d42c5c787e986d52473af3e8cc8e081fd04e785
Bart <bartpetka@gmail.com>
2018-10-16 14:56:47 +0200

    Fix imports

--- src/main/java/com/codecool/controller/WebController.java
+++ src/main/java/com/codecool/controller/WebController.java
@@ -1,15 +1,18 @@
 package com.codecool.controller;
 
 import com.codecool.exception.MalformedQueryException;
+import com.codecool.model.Entry;
 import com.codecool.model.UserQuery;
 import com.codecool.service.dataManipulation.SelectData;
 import com.codecool.service.queryValidation.QueryValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 @Controller
 public class WebController {
